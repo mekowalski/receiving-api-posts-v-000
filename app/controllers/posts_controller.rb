f70ab566e197e513c448a33e5dd6a9d5e698a666
Malind Kowalski <malind.nikulski@gmail.com>
2017-06-08 15:19:35 -0600

    learned receiving api post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to post_path(@post)
+    redirect_to post_path(@post), status: 201
   end
 
   def edit
